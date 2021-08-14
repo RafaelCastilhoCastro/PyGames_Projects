2ade14466c1748acd903d8e28d8047869eea4379
Rafael Castro <raphaeldavant@hotmail.com>
2021-08-14 00:25:53 -0300

    Add background scroll

--- space_flying.py
+++ space_flying.py
@@ -14,7 +14,7 @@ white = pygame.Color(255, 255, 255)
 grey = pygame.Color(128, 128, 128)
 blue = pygame.Color(0, 0, 135)
 
-pygame.display.set_caption("Spacefly")
+pygame.display.set_caption("Space flying")
 
 screen_width = 800
 screen_height = 600
@@ -27,8 +27,6 @@ font = pygame.font.SysFont("Verdana", 100)
 font_small = pygame.font.SysFont("Verdana", 30)
 game_over = font.render("Game Over", True, white)
 game_over_w, game_over_h = font.size('Game Over')
-
-background_img = pygame.image.load("images/stars_bg2a.png")
 
 
 class Enemy(pygame.sprite.Sprite):
@@ -43,7 +41,7 @@ class Enemy(pygame.sprite.Sprite):
         self.rect.y = random.randint(0, screen_height/2) + random.randint(0, screen_height/2)
         self.x_spd = random.randint(1, 5) * random.randint(1, 3)
         self.y_spd = 0
-        self.type = random.randint(1, 3)        #Type 0 horizontal. Type 1 ascending. Type 2 descending.
+        self.type = random.randint(1, 3)        # Type 0 horizontal. Type 1 ascending. Type 2 descending.
 
     def move(self):
         self.rect.move_ip(((self.x_spd * speed)/2) * -1, 0)
@@ -73,32 +71,61 @@ class Enemy(pygame.sprite.Sprite):
             self.x_spd = random.randint(1, 5) * random.randint(1, 3)
             self.rect.center = (screen_width + self.surf.get_width(), (random.randint(0, screen_height)))
 
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("images/player_pos0.png")
+        self.image = pygame.image.load("images/player_pose0.png")
         self.surf = pygame.Surface((90, 40))
         self.rect = self.surf.get_rect(center=(self.surf.get_width()/2, screen_height/2))
         self.spin = 0
 
+    """
+    # KEYBOARD-BASED PLAYER MOVEMENT & ANIMATION
+    def move(self):
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[K_UP] and self.rect.top >= 0:
+            self.rect.move_ip(0, -5)
+            if self.spin > -3:
+                self.spin -= 0.3
+        elif pressed_keys[K_DOWN] and self.rect.bottom <= 600:
+            self.rect.move_ip(0, 5)
+            if self.spin < 3:
+                self.spin += 0.3
+        elif pressed_keys[K_LEFT] and self.rect.left >= 0:
+            self.rect.move_ip(-5, 0)
+        elif pressed_keys[K_RIGHT] and self.rect.right <= 800:
+            self.rect.move_ip(5, 0)
 
-    # def move(self):           # KEYBOARD-BASED PLAYER MOVEMENT & ANIMATION
-        # pressed_keys = pygame.key.get_pressed()
-        # if pressed_keys[K_UP] and self.rect.top >= 0:
-        #     self.rect.move_ip(0, -5)
-        #     if self.spin > -3:
-        #         self.spin -= 0.3
-        # elif pressed_keys[K_DOWN] and self.rect.bottom <= 600:
-        #     self.rect.move_ip(0, 5)
-        #     if self.spin < 3:
-        #         self.spin += 0.3
-        # elif pressed_keys[K_LEFT] and self.rect.left >= 0:
-        #     self.rect.move_ip(-5, 0)
-        # elif pressed_keys[K_RIGHT] and self.rect.right <= 800:
-        #     self.rect.move_ip(5, 0)
+        player_pose = "images/player_pose{}.png".format(str(int(self.spin)))
+        self.image = pygame.image.load(player_pose)
+    """
 
-        # player_pos = "images/player_pos{}.png".format(str(int(self.spin)))
-        # self.image = pygame.image.load(player_pos)
+
+class Background():
+    def __init__(self):
+        self.bg_img = pygame.image.load("images/stars_bg1.png")
+        self.bg_rect = self.bg_img.get_rect()
+
+        self.bg1_y = 0
+        self.bg1_x = 0
+
+        self.bg2_y = 0
+        self.bg2_x = self.bg_rect.width
+
+        self.bg_spd = 1
+
+    def move(self):
+        self.bg1_x -= self.bg_spd
+        self.bg2_x -= self.bg_spd
+        if self.bg1_x <= -self.bg_rect.width:
+            self.bg1_x = self.bg_rect.width
+        if self.bg2_x <= -self.bg_rect.width:
+            self.bg2_x = self.bg_rect.width
+
+    def render(self):
+        display_surf.blit(self.bg_img, (self.bg1_x, self.bg1_y))
+        display_surf.blit(self.bg_img, (self.bg2_x, self.bg2_y))
 
 
 P1 = Player()
@@ -113,14 +140,13 @@ E8 = Enemy()
 
 enemies = pygame.sprite.Group()
 enemies.add(E1)
-# enemies.add(E2)
-# enemies.add(E3)
-# enemies.add(E4)
-# enemies.add(E5)
-# enemies.add(E6)
-# enemies.add(E7)
-# enemies.add(E8)
-
+enemies.add(E2)
+enemies.add(E3)
+enemies.add(E4)
+enemies.add(E5)
+enemies.add(E6)
+enemies.add(E7)
+enemies.add(E8)
 
 all_sprites = pygame.sprite.Group()
 all_sprites.add(P1)
@@ -132,6 +158,9 @@ all_sprites.add(E5)
 all_sprites.add(E6)
 all_sprites.add(E7)
 all_sprites.add(E8)
+
+bg_obj = Background()
+
 
 # INCREASE GAME SPEED & SCORE
 inc_speed = pygame.USEREVENT + 1
@@ -157,19 +186,21 @@ while True:
             pygame.mouse.set_visible(False)
             pygame.event.set_grab(True)
             P1.rect.center = (event.pos[0], event.pos[1])
-            mouse_pos = pygame.mouse.get_rel()[1]
-            if mouse_pos < 0:
+            mouse_y_pos = pygame.mouse.get_rel()[1]
+            if mouse_y_pos < 0:
                 if P1.spin > -3:
                     P1.spin -= 0.08
-            elif mouse_pos > 0:
+            elif mouse_y_pos > 0:
                 if P1.spin < 3:
                     P1.spin += 0.08
 
-            player_pos = "images/player_pos{}.png".format(str(int(P1.spin)))
-            P1.image = pygame.image.load(player_pos)
+            player_pose = "images/player_pose{}.png".format(str(int(P1.spin)))
+            P1.image = pygame.image.load(player_pose)
 
+    bg_obj.move()
+    bg_obj.render()
 
-    display_surf.blit(background_img, (0, 0))
+    # DRAW SCORE
     scores_text = font_small.render(str(score), True, white)
     display_surf.blit(scores_text, (10, 10))
 
@@ -191,12 +222,10 @@ while True:
         pygame.quit()
         sys.exit()
 
-
     pygame.display.update()
     FPS.tick(fps_max)
 
 """
-sprite changes through mouse movement
 
 Multiple lives or health bar
 
@@ -204,8 +233,8 @@ Add audio
     ambient
     player moves
 
-scrolling Background generation
 
+collision between enemies
 
 
 Done:
